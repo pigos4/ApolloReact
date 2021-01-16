1263ea22ec36d5bc66f623769c35519ea2ff5ff4
Daniel <64493498+pigos4@users.noreply.github.com>
2021-01-16 14:09:52 +0000

    route fixed

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { ApolloServer, gql } = require('apollo-server');
-const sessions = require('./data/sessions.json')
 
 const typeDefs = gql`type Query{
-    sessions:[Session],
-    ciao: [Ciao]
+        ciao: [Ciao]
     }
     type User {
     id: ID!
@@ -18,27 +16,14 @@ const typeDefs = gql`type Query{
     }
     
     type Mutation{addUser(id: ID, name: String, username: String, email: String): User}
-    
-type Session{
-    id:ID!
-    title: String!,
-    description:String,
-    startsAt:String,
-    endsAt:String,
-    room:String,
-    day:String,
-    format:String,
-    track:String,
-    level:String
-}`;
+   `; 
 
 const resolvers ={
     Mutation :{addUser: (root, args, context, info) => {
         console.log(args)
         return{id: 1, name: "dd", username:"gg", email: "ff"}
       }},
-   // Mutation:{users:(x,y,z,g,h)=>console.log(x,"x",y,"y",z,"z",g,"g",h)},
-    Query:{sessions:()=>{return sessions},ciao:()=>[{"id":1,"nome":"Daniel","cognome":"piga"},{"id":2,"nome":"jack","cognome":"boo"}]}
+    Query:{ciao:()=>[{"id":1,"nome":"Daniel","cognome":"piga"},{"id":2,"nome":"jack","cognome":"boo"}]}
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
